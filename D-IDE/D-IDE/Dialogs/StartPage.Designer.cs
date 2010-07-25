cfc953c6680d39f720bf75fe0b3674889f951536
artificialmind <artificialmind@2a2eaefd-8553-41d0-8860-e91f2a01d1c3>
2010-07-25 19:16:10 +0000

    Fixed DownloadStringAsync bug

--- D-IDE/D-IDE/Dialogs/StartPage.Designer.cs
+++ D-IDE/D-IDE/Dialogs/StartPage.Designer.cs
@@ -27,194 +27,207 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.lastProjects = new System.Windows.Forms.ListBox();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.lastFiles = new System.Windows.Forms.ListBox();
-			this.groupBox3 = new System.Windows.Forms.GroupBox();
-			this.web = new System.Windows.Forms.WebBrowser();
-			this.chTimer = new System.Windows.Forms.Timer(this.components);
-			this.doCheckAtStartCBox = new System.Windows.Forms.CheckBox();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
-			this.groupBox1.SuspendLayout();
-			this.groupBox2.SuspendLayout();
-			this.groupBox3.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Controls.Add(this.lastProjects);
-			this.groupBox1.Location = new System.Drawing.Point(12, 12);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(284, 268);
-			this.groupBox1.TabIndex = 0;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Last opened projects";
-			// 
-			// lastProjects
-			// 
-			this.lastProjects.BackColor = System.Drawing.Color.White;
-			this.lastProjects.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.lastProjects.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.lastProjects.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lastProjects.IntegralHeight = false;
-			this.lastProjects.ItemHeight = 18;
-			this.lastProjects.Items.AddRange(new object[] {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lastProjects = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lastFiles = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.web = new System.Windows.Forms.WebBrowser();
+            this.chTimer = new System.Windows.Forms.Timer(this.components);
+            this.doCheckAtStartCBox = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lNews = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lastProjects);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 268);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Last opened projects";
+            // 
+            // lastProjects
+            // 
+            this.lastProjects.BackColor = System.Drawing.Color.White;
+            this.lastProjects.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lastProjects.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lastProjects.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastProjects.IntegralHeight = false;
+            this.lastProjects.ItemHeight = 18;
+            this.lastProjects.Items.AddRange(new object[] {
             "Test",
             "A",
             "B"});
-			this.lastProjects.Location = new System.Drawing.Point(3, 16);
-			this.lastProjects.Name = "lastProjects";
-			this.lastProjects.Size = new System.Drawing.Size(278, 249);
-			this.lastProjects.TabIndex = 0;
-			this.lastProjects.DoubleClick += new System.EventHandler(this.lastProjects_DoubleClick);
-			// 
-			// groupBox2
-			// 
-			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)));
-			this.groupBox2.Controls.Add(this.lastFiles);
-			this.groupBox2.Location = new System.Drawing.Point(12, 317);
-			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(284, 280);
-			this.groupBox2.TabIndex = 1;
-			this.groupBox2.TabStop = false;
-			this.groupBox2.Text = "Last opened files";
-			// 
-			// lastFiles
-			// 
-			this.lastFiles.BackColor = System.Drawing.Color.White;
-			this.lastFiles.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.lastFiles.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.lastFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lastFiles.IntegralHeight = false;
-			this.lastFiles.ItemHeight = 18;
-			this.lastFiles.Items.AddRange(new object[] {
+            this.lastProjects.Location = new System.Drawing.Point(3, 16);
+            this.lastProjects.Name = "lastProjects";
+            this.lastProjects.Size = new System.Drawing.Size(278, 249);
+            this.lastProjects.TabIndex = 0;
+            this.lastProjects.DoubleClick += new System.EventHandler(this.lastProjects_DoubleClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.lastFiles);
+            this.groupBox2.Location = new System.Drawing.Point(12, 317);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(284, 280);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Last opened files";
+            // 
+            // lastFiles
+            // 
+            this.lastFiles.BackColor = System.Drawing.Color.White;
+            this.lastFiles.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lastFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lastFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastFiles.IntegralHeight = false;
+            this.lastFiles.ItemHeight = 18;
+            this.lastFiles.Items.AddRange(new object[] {
             "Test",
             "A",
             "B"});
-			this.lastFiles.Location = new System.Drawing.Point(3, 16);
-			this.lastFiles.Name = "lastFiles";
-			this.lastFiles.Size = new System.Drawing.Size(278, 261);
-			this.lastFiles.TabIndex = 0;
-			this.lastFiles.DoubleClick += new System.EventHandler(this.lastFiles_DoubleClick);
-			// 
-			// groupBox3
-			// 
-			this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox3.Controls.Add(this.web);
-			this.groupBox3.Location = new System.Drawing.Point(302, 12);
-			this.groupBox3.Name = "groupBox3";
-			this.groupBox3.Size = new System.Drawing.Size(529, 559);
-			this.groupBox3.TabIndex = 2;
-			this.groupBox3.TabStop = false;
-			this.groupBox3.Text = "Latest news from d-ide.sourceforge.net";
-			// 
-			// web
-			// 
-			this.web.AllowNavigation = false;
-			this.web.AllowWebBrowserDrop = false;
-			this.web.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.web.IsWebBrowserContextMenuEnabled = false;
-			this.web.Location = new System.Drawing.Point(3, 16);
-			this.web.MinimumSize = new System.Drawing.Size(20, 20);
-			this.web.Name = "web";
-			this.web.Size = new System.Drawing.Size(523, 540);
-			this.web.TabIndex = 1;
-			this.web.WebBrowserShortcutsEnabled = false;
-			// 
-			// chTimer
-			// 
-			this.chTimer.Interval = 1000;
-			this.chTimer.Tick += new System.EventHandler(this.chTimer_Tick);
-			// 
-			// doCheckAtStartCBox
-			// 
-			this.doCheckAtStartCBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.doCheckAtStartCBox.AutoSize = true;
-			this.doCheckAtStartCBox.Location = new System.Drawing.Point(305, 577);
-			this.doCheckAtStartCBox.Name = "doCheckAtStartCBox";
-			this.doCheckAtStartCBox.Size = new System.Drawing.Size(141, 17);
-			this.doCheckAtStartCBox.TabIndex = 3;
-			this.doCheckAtStartCBox.Text = "Retrieve news at startup";
-			this.doCheckAtStartCBox.UseVisualStyleBackColor = true;
-			this.doCheckAtStartCBox.CheckedChanged += new System.EventHandler(this.doCheckAtStartCBox_CheckedChanged);
-			// 
-			// button2
-			// 
-			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.button2.Location = new System.Drawing.Point(753, 574);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 5;
-			this.button2.Text = "Close";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(12, 286);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(140, 23);
-			this.button1.TabIndex = 6;
-			this.button1.Text = "Create Project";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click_1);
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(156, 286);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(140, 23);
-			this.button3.TabIndex = 7;
-			this.button3.Text = "Open Selected";
-			this.button3.UseVisualStyleBackColor = true;
-			this.button3.Click += new System.EventHandler(this.lastProjects_DoubleClick);
-			// 
-			// button4
-			// 
-			this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.button4.Location = new System.Drawing.Point(672, 574);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(75, 23);
-			this.button4.TabIndex = 8;
-			this.button4.Text = "Refresh";
-			this.button4.UseVisualStyleBackColor = true;
-			this.button4.Click += new System.EventHandler(this.button4_Click);
-			// 
-			// StartPage
-			// 
-			this.AcceptButton = this.button2;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.White;
-			this.ClientSize = new System.Drawing.Size(843, 611);
-			this.Controls.Add(this.button4);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.doCheckAtStartCBox);
-			this.Controls.Add(this.groupBox3);
-			this.Controls.Add(this.groupBox2);
-			this.Controls.Add(this.groupBox1);
-			this.DoubleBuffered = true;
-			this.HideOnClose = true;
-			this.Name = "StartPage";
-			this.ShowHint = WeifenLuo.WinFormsUI.Docking.DockState.Document;
-			this.TabText = "StartPage";
-			this.Text = "StartPage";
-			this.Shown += new System.EventHandler(this.StartPage_Shown);
-			this.groupBox1.ResumeLayout(false);
-			this.groupBox2.ResumeLayout(false);
-			this.groupBox3.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.lastFiles.Location = new System.Drawing.Point(3, 16);
+            this.lastFiles.Name = "lastFiles";
+            this.lastFiles.Size = new System.Drawing.Size(278, 261);
+            this.lastFiles.TabIndex = 0;
+            this.lastFiles.DoubleClick += new System.EventHandler(this.lastFiles_DoubleClick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.lNews);
+            this.groupBox3.Controls.Add(this.web);
+            this.groupBox3.Location = new System.Drawing.Point(302, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(529, 559);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Latest news from d-ide.sourceforge.net";
+            // 
+            // web
+            // 
+            this.web.AllowNavigation = false;
+            this.web.AllowWebBrowserDrop = false;
+            this.web.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.web.IsWebBrowserContextMenuEnabled = false;
+            this.web.Location = new System.Drawing.Point(3, 16);
+            this.web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.web.Name = "web";
+            this.web.Size = new System.Drawing.Size(523, 540);
+            this.web.TabIndex = 1;
+            this.web.WebBrowserShortcutsEnabled = false;
+            // 
+            // chTimer
+            // 
+            this.chTimer.Interval = 1000;
+            this.chTimer.Tick += new System.EventHandler(this.chTimer_Tick);
+            // 
+            // doCheckAtStartCBox
+            // 
+            this.doCheckAtStartCBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.doCheckAtStartCBox.AutoSize = true;
+            this.doCheckAtStartCBox.Location = new System.Drawing.Point(305, 577);
+            this.doCheckAtStartCBox.Name = "doCheckAtStartCBox";
+            this.doCheckAtStartCBox.Size = new System.Drawing.Size(141, 17);
+            this.doCheckAtStartCBox.TabIndex = 3;
+            this.doCheckAtStartCBox.Text = "Retrieve news at startup";
+            this.doCheckAtStartCBox.UseVisualStyleBackColor = true;
+            this.doCheckAtStartCBox.CheckedChanged += new System.EventHandler(this.doCheckAtStartCBox_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(753, 574);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Create Project";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 286);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Open Selected";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.lastProjects_DoubleClick);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(672, 574);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Refresh";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lNews
+            // 
+            this.lNews.AutoSize = true;
+            this.lNews.Location = new System.Drawing.Point(6, 16);
+            this.lNews.Name = "lNews";
+            this.lNews.Size = new System.Drawing.Size(107, 13);
+            this.lNews.TabIndex = 2;
+            this.lNews.Text = "downloading news ...";
+            this.lNews.Visible = false;
+            // 
+            // StartPage
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(843, 611);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.doCheckAtStartCBox);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.HideOnClose = true;
+            this.Name = "StartPage";
+            this.ShowHint = WeifenLuo.WinFormsUI.Docking.DockState.Document;
+            this.TabText = "StartPage";
+            this.Text = "StartPage";
+            this.Shown += new System.EventHandler(this.StartPage_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -232,5 +245,6 @@
 		private System.Windows.Forms.Button button3;
 		public System.Windows.Forms.WebBrowser web;
 		private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lNews;
 	}
 }
--- D-IDE/D-IDE/Dialogs/StartPage.cs
+++ D-IDE/D-IDE/Dialogs/StartPage.cs
@@ -15,136 +15,150 @@ using System.Net;
 
 namespace D_IDE
 {
-	public partial class StartPage : DockContent
-	{
-		public StartPage()
-		{
-			InitializeComponent();
-			DockAreas = DockAreas.Document;
+    public partial class StartPage : DockContent
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+            DockAreas = DockAreas.Document;
 
-			webclient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webclient_DownloadStringCompleted);
+            doCheckAtStartCBox.Checked = D_IDE_Properties.Default.RetrieveNews;
+        }
 
-			doCheckAtStartCBox.Checked = D_IDE_Properties.Default.RetrieveNews;
-		}
+        WebClient webclient = new WebClient();
+        private bool downloading = false;
+        /// <summary>
+        /// Creates a seperate thread for news retrieval due to bug in DownloadStringAsync
+        /// </summary>
+        public void UpdateNews()
+        {
+            if (!webclient.IsBusy && !downloading)
+            {
+                downloading = true;
+                Control.CheckForIllegalCrossThreadCalls = false;
+                lNews.Show();
+                new Thread(() =>
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    ParseNews(webclient.DownloadString(new Uri(Program.news_php + "?xml=1&max=30&fromIDE=1")));
+                    downloading = false;
+                    lNews.Hide();
+                }).Start();
+            }
+        }
 
-		WebClient webclient = new WebClient();
-		public void UpdateNews()
-		{
-			if (!webclient.IsBusy)
-				webclient.DownloadStringAsync(new Uri(Program.news_php + "?xml=1&max=30&fromIDE=1"));
-		}
+        private void ParseNews(string result)
+        {
+            List<NewsEntry> nl = new List<NewsEntry>();
+            try
+            {
+                nl.Clear();
+                XmlTextReader xtr = new XmlTextReader(new StringReader(result));
 
-		void webclient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
-		{
-			List<NewsEntry> nl = new List<NewsEntry>();
-			try
-			{
-				nl.Clear();
-				XmlTextReader xtr = new XmlTextReader(new StringReader(e.Result));
+                xtr.ReadStartElement(); // Skip initial element
 
-				xtr.ReadStartElement(); // Skip initial element
+                while (xtr.Name == "n" && xtr.NodeType == XmlNodeType.Element)
+                {
+                    int nid = Convert.ToInt32(xtr.GetAttribute("id"));
+                    long timestamp = Convert.ToInt64(xtr.GetAttribute("timestamp"));
+                    string content = xtr.ReadElementContentAsString();
 
-				while (xtr.Name == "n" && xtr.NodeType == XmlNodeType.Element)
-				{
-					int nid = Convert.ToInt32(xtr.GetAttribute("id"));
-					long timestamp = Convert.ToInt64(xtr.GetAttribute("timestamp"));
-					string content = xtr.ReadElementContentAsString();
-
-					nl.Add(new NewsEntry(nid, D_IDE_Properties.DateFromUnixTime(timestamp).ToLocalTime(), content));
-				}
-				News = nl;
-			}
-			catch (Exception ex) { D_IDEForm.thisForm.Log(ex.Message); }
-		}
+                    nl.Add(new NewsEntry(nid, D_IDE_Properties.DateFromUnixTime(timestamp).ToLocalTime(), content));
+                }
+                News = nl;
+            }
+            catch (Exception ex) { D_IDEForm.thisForm.Log(ex.Message); }
+        }
 
 
-		public List<NewsEntry> News
-		{
-			set
-			{
-				string c = "<html><body><style>body{ font-size:11px; font-family:Arial;} " +
-					".a{font-weight:bold;}" +
-					".n{display:block;padding-bottom:15px;}" +
-					"p{margin:0;padding:0;}" +
-					"</style>";
-				foreach (NewsEntry n in value)
-				{
-					c += "<div class=\"n\"><span class=\"a\">" + n.time.ToString() + "</span><br />" + n.content + "</div>";
-				}
-				c += "</body></html>";
-				web.DocumentText = c;
-			}
-		}
+        public List<NewsEntry> News
+        {
+            set
+            {
+                string c = "<html><body><style>body{ font-size:11px; font-family:Arial;} " +
+                    ".a{font-weight:bold;}" +
+                    ".n{display:block;padding-bottom:15px;}" +
+                    "p{margin:0;padding:0;}" +
+                    "</style>";
+                foreach (NewsEntry n in value)
+                {
+                    c += "<div class=\"n\"><span class=\"a\">" + n.time.ToString() + "</span><br />" + n.content + "</div>";
+                }
+                c += "</body></html>";
+                
+                web.DocumentText = c;
+            }
+        }
 
-		private void lastProjects_DoubleClick(object sender, EventArgs e)
-		{
-			if (lastProjects.SelectedItems.Count > 0)
-				D_IDEForm.thisForm.Open(D_IDE_Properties.Default.lastProjects[lastProjects.SelectedIndex]);
-		}
+        private void lastProjects_DoubleClick(object sender, EventArgs e)
+        {
+            if (lastProjects.SelectedItems.Count > 0)
+                D_IDEForm.thisForm.Open(D_IDE_Properties.Default.lastProjects[lastProjects.SelectedIndex]);
+        }
 
-		private void lastFiles_DoubleClick(object sender, EventArgs e)
-		{
-			if (lastFiles.SelectedItems.Count > 0)
-				D_IDEForm.thisForm.Open(D_IDE_Properties.Default.lastFiles[lastFiles.SelectedIndex]);
-		}
+        private void lastFiles_DoubleClick(object sender, EventArgs e)
+        {
+            if (lastFiles.SelectedItems.Count > 0)
+                D_IDEForm.thisForm.Open(D_IDE_Properties.Default.lastFiles[lastFiles.SelectedIndex]);
+        }
 
-		private void chTimer_Tick(object sender, EventArgs e)
-		{
-			//if (newsTh.IsAlive) return;
-			//News = nl;
-			chTimer.Stop();
-		}
+        private void chTimer_Tick(object sender, EventArgs e)
+        {
+            //if (newsTh.IsAlive) return;
+            //News = nl;
+            chTimer.Stop();
+        }
 
-		private void button2_Click(object sender, EventArgs e)
-		{
+        private void button2_Click(object sender, EventArgs e)
+        {
             Hide();
-		}
+        }
 
-		private void button4_Click(object sender, EventArgs e)
-		{
-			UpdateNews();
-		}
+        private void button4_Click(object sender, EventArgs e)
+        {
+            UpdateNews();
+        }
 
-		private void doCheckAtStartCBox_CheckedChanged(object sender, EventArgs e)
-		{
-			D_IDE_Properties.Default.RetrieveNews = doCheckAtStartCBox.Checked;
-		}
+        private void doCheckAtStartCBox_CheckedChanged(object sender, EventArgs e)
+        {
+            D_IDE_Properties.Default.RetrieveNews = doCheckAtStartCBox.Checked;
+        }
 
-		private void button1_Click_1(object sender, EventArgs e)
-		{
-			D_IDEForm.thisForm.NewProject(sender, e);
-		}
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            D_IDEForm.thisForm.NewProject(sender, e);
+        }
 
-		private void StartPage_Shown(object sender, EventArgs e)
-		{
-			if (doCheckAtStartCBox.Checked)
-			{
-				BeginInvoke(new EventHandler(delegate(object s, EventArgs ea)
-					{
-						UpdateNews();
-					}), null, EventArgs.Empty);
-			}
-		}
-	}
+        private void StartPage_Shown(object sender, EventArgs e)
+        {
+            if (doCheckAtStartCBox.Checked)
+            {
+                BeginInvoke(new EventHandler(delegate(object s, EventArgs ea)
+                    {
+                        UpdateNews();
+                    }), null, EventArgs.Empty);
+            }
+        }
+    }
 
-	public struct NewsEntry
-	{
-		public int id;
-		public DateTime time;
-		public string content;
+    public struct NewsEntry
+    {
+        public int id;
+        public DateTime time;
+        public string content;
 
-		public NewsEntry(int id, DateTime time, string content)
-		{
-			this.id = id;
-			this.time = time;
-			this.content = content;
-		}
+        public NewsEntry(int id, DateTime time, string content)
+        {
+            this.id = id;
+            this.time = time;
+            this.content = content;
+        }
 
-		public NewsEntry(DateTime time, string content)
-		{
-			this.id = 1;
-			this.time = time;
-			this.content = content;
-		}
-	}
+        public NewsEntry(DateTime time, string content)
+        {
+            this.id = 1;
+            this.time = time;
+            this.content = content;
+        }
+    }
 }
