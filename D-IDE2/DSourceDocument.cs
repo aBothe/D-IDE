21d2f28e0dbc080255c4428c3458d4cd41e84abe
abothe <abothe@2a2eaefd-8553-41d0-8860-e91f2a01d1c3>
2010-12-05 17:30:57 +0000

    WPF-based D-IDE2 has been initiated :D

---
+++ D-IDE2/DSourceDocument.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AvalonDock;
+using ICSharpCode.AvalonEdit;
+
+namespace D_IDE
+{
+	class DSourceDocument : DockableContent
+	{
+		public readonly TextEditor Editor = new ICSharpCode.AvalonEdit.TextEditor();
+
+		public DSourceDocument()
+		{
+			AddChild(Editor);
+
+			Init();
+
+			Editor.Document.Text = "Hallo Welt!";
+		}
+
+		void Init()
+		{
+			Editor.ShowLineNumbers = true;
+		}
+	}
+}
---
+++ D-IDE2/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Windows.Controls.Ribbon;
+
+namespace D_IDE
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : RibbonWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var src = new DSourceDocument();
+			src.ShowAsDocument(DockMgr);
+		}
+
+		private void NewSource(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void NewProject(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Open(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Save(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void SaveAs(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Exit(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Settings(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
