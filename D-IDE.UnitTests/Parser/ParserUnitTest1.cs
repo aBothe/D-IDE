9c82c446875aa9c9ec35d7d520d8224710ccb0f2
komma8komma1 <komma8komma1@2a2eaefd-8553-41d0-8860-e91f2a01d1c3>
2011-02-16 04:30:15 +0000

    Added visual studio unit test project. Added simple unit test that tries to parse phobos. More practical tests will be small chunks of code with relatively small results we can compare to. This will help prevent regression errors in the future.

---
+++ D-IDE.UnitTests/Parser/ParserUnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using D_Parser;
+using Parser.Core;
+using System.IO;
+
+namespace D_IDE.UnitTests
+{
+    /// <summary>
+    /// A spot for parser unit tests.
+    /// </summary>
+    [TestClass]
+    public class ParserUnitTest1
+    {
+        protected static DirectoryInfo phobosDir;
+        protected static DirectoryInfo dmdDir;
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContextInstance) 
+        {
+            DParser.OnError += delegate(IAbstractSyntaxTree syntaxTree, int line, int col, int kindOf, string message) 
+                {
+                    Assert.Fail("File \"" + syntaxTree.FileName + "\" - (Ln "+line+", Col " + col + ") [" + kindOf + "] " + message);
+                };
+
+            dmdDir = new DirectoryInfo(D.DSettings.Instance.dmd2.BaseDirectory);
+            if (!dmdDir.Exists)
+            {
+                dmdDir = new DirectoryInfo(@"c:\d\dmd2");
+                D.DSettings.Instance.dmd2.BaseDirectory = dmdDir.FullName;
+            }
+            phobosDir = new DirectoryInfo(D.DSettings.Instance.dmd2.BaseDirectory + @"\src\phobos");
+        }
+
+        [ClassCleanup()]
+        public static void Cleanup() { }
+        
+        [TestMethod]
+        public void ParsePhobos()
+        {
+            FileInfo[] files = phobosDir.GetFiles("*.d", SearchOption.AllDirectories);
+
+            foreach (FileInfo f in files)
+            {
+                IAbstractSyntaxTree syntaxTree = DParser.ParseFile(f.FullName);
+                Assert.IsNotNull(syntaxTree, "The syntax tree was not instantiated!");
+            }
+        }
+    }
+}
---
+++ D-IDE.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("D-IDE.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("D-IDE.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f427492-ea2d-491d-95a6-8d8aa7ec826b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
