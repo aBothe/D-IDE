f5f1669780aa92fc6a06801c493bc117e3ce3ead
Alexander Bothe <a_bothe@gmx.net>
2012-04-01 03:16:39 +0200

    [Resolver] First rough implementation attempt of ufcs resolution.

--- DParser2/Resolver/TypeResolution/UFCSResolver.cs
+++ DParser2/Resolver/TypeResolution/UFCSResolver.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using D_Parser.Dom.Expressions;
+using D_Parser.Resolver.ASTScanner;
+using D_Parser.Dom;
 
 namespace D_Parser.Resolver.TypeResolution
 {
@@ -15,5 +18,64 @@ namespace D_Parser.Resolver.TypeResolution
 	/// </summary>
 	public class UFCSResolver
 	{
+		public static ResolveResult[] TryResolveUFCS(
+			ResolveResult firstArgument, 
+			PostfixExpression_Access acc, 
+			ResolverContextStack ctxt)
+		{
+			var name="";
+
+			if (acc.AccessExpression is IdentifierExpression)
+				name = ((IdentifierExpression)acc.AccessExpression).Value as string;
+			else if (acc.AccessExpression is TemplateInstanceExpression)
+				name = ((TemplateInstanceExpression)acc.AccessExpression).TemplateIdentifier.Id;
+			else
+				return null;
+
+
+			var vis = new UFCSVisitor(ctxt) {
+				FirstParamToCompareWith=firstArgument,
+				NameToSearch=name
+			};
+
+			vis.IterateThroughScopeLayers(acc.Location);
+
+			if (vis.Match != null)
+				return new[] {TypeDeclarationResolver.HandleNodeMatch(vis.Match, ctxt, null, acc)};
+
+			return null;
+		}
+
+		public class UFCSVisitor : AbstractVisitor
+		{
+			public UFCSVisitor(ResolverContextStack ctxt) : base(ctxt) { }
+
+			public string NameToSearch;
+			public ResolveResult FirstParamToCompareWith;
+
+			public DMethod Match;
+
+			protected override bool HandleItem(INode n)
+			{
+				if (n.Name == NameToSearch && n is DMethod)
+				{
+					var dm = (DMethod)n;
+
+					if (dm.Parameters.Count != 0)
+					{
+						var firstParam = TypeResolution.TypeDeclarationResolver.Resolve(dm.Parameters[0].Type,Context);
+
+						//TODO: Compare the resolved parameter with the first parameter given
+						if (true)
+						{
+							Match = dm;
+							return true;
+						}
+					}
+				}
+
+				return false;
+			}
+		}
 	}
 }
